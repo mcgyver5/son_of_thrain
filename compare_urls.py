9295492f6cb550f2d05b336af4e75d229727c59e
Tim <tim.e.mcguire@gmail.com>
2020-01-13 13:58:00 -0600

    Create compare_urls.py
    comment to say how the file should work

---
+++ compare_urls.py
@@ -0,0 +1,9 @@
+"""
+1. open file
+2. pull all urls into map or list via regex
+3. use git API to look at remote file  from github / gitlab
+4. make list of urls from that file
+5. result is format like 'Your file is missing the following URLs that the remote repo has and your file has the following urls that the 
+remote repo is missing.'
+
+"""
