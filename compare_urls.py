142cc329c1498c49bd213c9603f9c48822136b47
mcgyver5 <tim.e.mcguire@gmail.com>
2020-01-15 18:12:44 +0000

    user contributes remote and local file

--- compare_urls.py
+++ compare_urls.py
@@ -7,34 +7,61 @@
 remote repo is missing.'
 
 """
+import os.path
+from os import path
+import sys
+import getopt
 import re
 import requests
 from collections import Counter
 
+def print_results(desc,results):
+    section_separator = "\r\n-----------------------------------------------------------------------------------\r\n"
+    result_string = "The {} list has the following {} URLs that the {} list does not have:"
+    if desc == "right":
+        result_string = result_string.format("remote",len(results),"local")
+    if desc == "left":
+        result_string = result_string.format("local",len(results),"remote")
+    if desc == "middle":
+        result_string = "The lists have the following {} URLs in common: ".format(len(results))
+    none_result = "The {} list is empty".format(desc)
+    if len(results) > 0:
+        print(result_string)
+        for url in results:
+            print(url)
+    else:
+        print(none_result)
+    print(section_separator)
+
 def find_url(string):
     url = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\), ]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', string) 
     return url
-
-fh = open("my_test_links.txt")
+local_file = ""
+if len(sys.argv) > 2:
+    local_file = sys.argv[1]
+    if path.exists(local_file):
+        print(local_file + " exists ")
+    else:
+        print(local_file + " does not exist")
+        sys.exit()
+else:
+    print("usage: python compare_urls.py local_file remote_file")
+    sys.exit()
 url_list = []
+fh = open(local_file)
 for line in fh:
     url = find_url(line)
     if url:
         url_list.append(url[0])
-for u in url_list:
-    print(u)
 
 remote_list = []
-remote_url = "https://raw.githubusercontent.com/sanjacinto/excellent_training_videos/master/README.md"
-#remote_url = "https://github.com/sanjacinto/excellent_training_videos/blob/master/README.md"
+remote_url = sys.argv[2]
+#remote_url = "https://raw.githubusercontent.com/sanjacinto/excellent_training_videos/master/README.md"
 r = requests.get(remote_url, allow_redirects=True)
 for s in r:
     url = find_url(s)
     if url:
         remote_list.append(url[0])
-
-for link in remote_list:
-    print(link)
 
 left_result = list((Counter(url_list) - Counter(remote_list)).elements())
 right_result = list((Counter(remote_list) - Counter(url_list)).elements())
@@ -43,10 +70,7 @@ local_set = set(url_list)
 
 middle_result = local_set & remote_set 
 
+print_results("right",right_result)
+print_results("left",left_result)
+print_results("middle",middle_result)
 
-print("the remote list has the following URLs that the local one does not have:")
-print(str(right_result))
-print("your list has the following URLS that the remote list does not have")
-print(str(left_result))
-print("both lists contain the following URLs:")
-print(str(middle_result))
